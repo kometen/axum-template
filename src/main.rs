54af82939e8f4e8303aca7dcc9539362860feb93
Claus Guttesen <kometen@gmail.com>
2022-10-15 23:05:26 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Jeg æder blåbærsyltetøj!" }));
+
+    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
